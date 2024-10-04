8de93d059df56cd8faecfb9a6f25cb712139be94
Mariana Marzayani Hernández Jurado <133036569+MarzyHernandez@users.noreply.github.com>
2024-10-04 09:03:13 -0600

    Update Cart.kt

--- App Android/app/src/main/java/mx/acg/zazil/model/Cart.kt
+++ App Android/app/src/main/java/mx/acg/zazil/model/Cart.kt
@@ -1,8 +1,12 @@
 package mx.acg.zazil.model
 
+import okhttp3.ResponseBody
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PUT
 import retrofit2.http.Query
 
 // Modelo del carrito
@@ -25,10 +29,31 @@ data class CartProduct(
     val cantidad: Int
 )
 
-data class AddToCartRequest(
+
+// Clase de datos que representa el cuerpo de la solicitud para actualizar el carrito
+data class CartUpdate(
     val uid: String,
     val id_producto: Int,
     val cantidad: Int
 )
 
 
+// Interfaz para la API del carrito
+interface CartApi {
+    @GET("/")
+    suspend fun getCartByUid(@Query("uid") uid: String): Cart
+
+    @PUT("https://updatecart-dztx2pd2na-uc.a.run.app")
+    suspend fun updateCart(@Body cartUpdate: CartUpdate): Response<ResponseBody>
+}
+
+// Singleton para Retrofit de la API del carrito
+object CartRetrofitInstance {
+    val cartApi: CartApi by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://getactivecart-dztx2pd2na-uc.a.run.app/") // Base URL de la API del carrito
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CartApi::class.java)
+    }
+}
